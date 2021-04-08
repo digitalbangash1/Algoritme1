76ebd066975936703f4c90c33e6c11d33daf3dc9
digitalbangash1 <s092512@student.dtu.dk>
2021-04-08 11:48:14 +0200

    vørsgo

--- src/FindMax/FindMax2.java
+++ src/FindMax/FindMax2.java
@@ -1,5 +1,27 @@
 package FindMax;
 
 public class FindMax2 {
-    static int array[]= {}
-}
+
+    static int array[] = {5, 7, 1, 0, 2, 4, 76, 43, 45};
+
+    static int largest() {
+
+        int max = array[0];
+
+        for (int i = 1; i < array.length; i++)
+            if (array[i] > max) max = array[i];
+            return max;
+
+
+        }
+
+    public static void main(String[] args) {
+        System.out.println("Maximum   ="  + largest());
+
+
+    }
+
+
+    }
+
+
